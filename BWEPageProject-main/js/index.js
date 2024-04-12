092a2711af4d3b69300e9998b53f796a4586584c
bale <va.latorre@duocuc.cl>
2024-04-11 23:45:13 -0400

    a

---
+++ BWEPageProject-main/js/index.js
@@ -0,0 +1,6 @@
+const btnAbrirModal = 
+document.querySelector("#btn-abrir-modal");
+const btnCerrarModal = 
+document.querySelector("#btn-cerrar-modal");
+const modal =
+document.querySelector("#modal");
