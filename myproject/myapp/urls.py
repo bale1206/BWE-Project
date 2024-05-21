0716b761ab007229d94cb33735b119fe9453df88
bale <va.latorre@duocuc.cl>
2024-05-21 19:48:19 -0400

    django changes

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path 
 from . import views
 
 urlpatterns = [
-    path('',views.index,name='index')
+    path('', views.home, name='myapp-home'),
+    path('about/', views.about, name='myapp-about'),
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-    context = {
-        'name': 'Patrick',
-        'age': 23,
-        'nationality': 'British'
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2018'
     }
-    return render(request, 'index.html',context)
+]
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'myapp/templates/blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'myapp/templates/blog/about.html',{'title': 'About'})
