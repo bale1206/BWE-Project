e764c72d2e4cc173f4498a1fcc10ddd4603c4040
bale <va.latorre@duocuc.cl>
2024-05-23 13:52:12 -0400

    changes thursday pt 2

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='myapp-home'),
-    path('about/', views.about, name='myapp-about'),
+    path('', views.index, name='index'),
+    path('explorer', views.explorer, name='explorer'),
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,26 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2018'
-    }
-]
-def home(request):
+
+def index(request):
     context = {
-        'posts': posts
     }
-    return render(request, 'myapp/templates/blog/home.html', context)
+    return render(request, 'pages/index.html', context)
 
 
-def about(request):
-    return render(request, 'myapp/templates/blog/about.html',{'title': 'About'})
+def explorer(request):
+    context = {}
+    return render(request, 'pages/explorer.html',context)
+
+def login(request):
+    context = {}
+    return render(request, 'pages/login.html', context)
+
