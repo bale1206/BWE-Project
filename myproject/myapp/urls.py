14bebed522a3ae876f874e2bf1dd620a2eaac9b0
bale <va.latorre@duocuc.cl>
2024-05-23 14:05:17 -0400

    changes pt3

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('explorer', views.explorer, name='explorer'),
+    path('login', views.login, name='login'),
+    path('profile', views.profile, name='profile'),
+    path('register', views.register, name='register'),
+    path('shoppingcart', views.shoppingcart, name='shoppingcart'),
+
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -3,10 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    context = {
-    }
+    context = {}
     return render(request, 'pages/index.html', context)
-
 
 def explorer(request):
     context = {}
@@ -15,4 +13,16 @@ def explorer(request):
 def login(request):
     context = {}
     return render(request, 'pages/login.html', context)
+
+def profile(request):
+    context = {}
+    return render(request, 'pages/profile.html', context)
+
+def register(request):
+    context = {}
+    return render(request, 'pages/register.html',context)
+
+def shoppingcart(request):
+    context = {}
+    return render(request, 'pages/shoppingcart.html', context)
 
