9b21d590cb48deb960bfac5b1dcb4772caa31164
bale <va.latorre@duocuc.cl>
2024-05-23 23:14:29 -0400

    thursday night changes pt2

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -4,26 +4,26 @@ from django.shortcuts import render
 
 def index(request):
     context = {}
-    return render(request, 'pages/index.html', context)
+    return render(request,'pages/index.html', context)
 
 def explorer(request):
     context = {}
-    return render(request, 'pages/explorer.html', context)
+    return render(request,'pages/explorer.html', context)
 
 def login(request):
     context = {}
-    return render(request, 'pages/login.html', context)
+    return render(request,'pages/login.html', context)
 
 def profile(request):
     context = {}
-    return render(request, 'pages/profile.html', context)
+    return render(request,'pages/profile.html', context)
 
 def register(request):
     context = {}
-    return render(request, 'pages/register.html', context)
+    return render(request,'pages/register.html', context)
 
 def shoppingcart(request):
     context = {}
-    return render(request, 'pages/shoppingcart.html', context)
+    return render(request,'pages/shoppingcart.html', context)
 
 
